7c6a2383ef239c53b39c53a6f765d2cecae6c9f5
krishma <krishmas536@gmail.com>
2021-10-12 23:51:46 -0700

    Done?

--- src/main/java/DifferenceOfSquaresCalculator.java
+++ src/main/java/DifferenceOfSquaresCalculator.java
@@ -1,15 +1,43 @@
 class DifferenceOfSquaresCalculator {
 
     int computeSquareOfSumTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        int sum = 0;
+        int squaredsum;
+
+        for(int i = 1; i <= input; i ++){
+            sum += i;
+        }
+        squaredsum = exponential(sum, 2);
+
+        return squaredsum;
     }
 
     int computeSumOfSquaresTo(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+        int sumSquares = 0;
+
+        for(int i = 1; i <= input; i ++){
+            sumSquares += exponential(i ,2);
+        }
+        return sumSquares;
     }
 
     int computeDifferenceOfSquares(int input) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+
+       int squaresum = computeSquareOfSumTo(input);
+       int sumsquares = computeSumOfSquaresTo(input);
+
+       int Difference = squaresum - sumsquares;
+
+       return Difference;
+    }
+
+    private int exponential(int x, int y){
+        if(y==0) {
+            return 1;
+        }
+        else return x*exponential(x,y-1);
     }
 
 }
